9c210d51cdc9e2633d5c46605fb3185e953054b5
DK <kd.000.000.1@gmail.com>
2023-02-15 13:43:11 -0600

    algorithm changed to to online next-fit

--- bin-packing-visualization/src/app/app.component.ts
+++ bin-packing-visualization/src/app/app.component.ts
@@ -1,80 +1,69 @@
-import { Component } from '@angular/core';
+// https://www.geeksforgeeks.org/bin-packing-problem-minimize-number-of-used-bins
 
-interface Rectangle {
-  width: number;
-  height: number;
-  x?: number;
-  y?: number;
-}
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  templateUrl: 'app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bin-packing-visualization';
-
   rectangles: Rectangle[] = [
-    { width: 200, height: 100 },
-    { width: 100, height: 200 },
-    { width: 50, height: 50 },
-    { width: 150, height: 150 }
-  ];
+    { width: 20, height: 5, color: 'red' },
+    { width: 7, height: 18, color: 'blue' },
+    { width: 7, height: 18, color: 'green' },
+    { width: 6, height: 32, color: 'yellow' },
+    { width: 48, height: 50, color: 'grey' },
+    { width: 3, height: 7, color: 'cyan' },
+    //{ width: 80, height: 60, color: 'black' },
+  ]
 
-  containerWidth = 500;
-  containerHeight = 500;
-
-  packedRectangles: Rectangle[] = [];
+  positions: { x: number, y: number }[] = [];
 
   ngOnInit() {
-    this.packedRectangles = this.packRectangles(this.rectangles, this.containerWidth, this.containerHeight);
-  }
+    const bins = this.packRectangles(this.rectangles, 50, 80);
 
-  packRectangles(rectangles: Rectangle[], containerWidth: number, containerHeight: number): Rectangle[] {
-    const sortedRectangles = [...rectangles.slice().sort((a, b) => b.height - a.height)];
-//     boxes.sort((a, b) => b.width * b.height - a.width * a.height);
-    const nodes = [{ x: 0, y: 0, width: containerWidth, height: containerHeight }] as Rectangle[];
-    const packedRectangles: Rectangle[] = [];
+    let x = 0, y = 0;
 
-    for (const rectangle of sortedRectangles) {
-      let i = 0;
-      while (i < nodes.length) {
-        const node = nodes[i];
-        if (rectangle.width <= node.width && rectangle.height <= node.height) {
-          const packedRectangle = {
-            x: node.x,
-            y: node.y,
-            width: rectangle.width,
-            height: rectangle.height,
-          };
-          packedRectangles.push(packedRectangle);
-          nodes.splice(i, 1, ...this.splitNode(node, rectangle));
-
-          break;
-        } else {
-          i++;
-        }
+    for (const bin of bins) {
+      for (const rect of bin) {
+        this.positions.push({ x, y });
+        x += rect?.width;
       }
-    }
 
-    return packedRectangles;
-  }
-
-  splitNode(node: Rectangle, rectangle: Rectangle): Rectangle[] {
-    const remainingWidth = node.width ?? 0 - rectangle.width;
-    const remainingHeight = node.height - rectangle.height;
-
-    if (remainingWidth > remainingHeight) {
-      return [
-        { x: (node?.x ?? 0), y: (node?.y ?? 0), width: (rectangle?.width ?? 0), height: (node?.height ?? 0) } as Rectangle,
-        { x: (node?.x ?? 0) + (rectangle?.width ?? 0), y: (node?.y ?? 0), width: remainingWidth ?? 0, height: (node?.height ?? 0) } as Rectangle,
-      ] as Rectangle[];
-    } else {
-      return [
-        { x: (node?.x ?? 0), y: (node?.y ?? 0), width: node.width, height: rectangle.height } as Rectangle,
-        { x: (node?.x ?? 0), y: (node?.y ?? 0) + (rectangle?.height ?? 0), width: (node?.width ?? 0), height: (remainingHeight ?? 0) } as Rectangle,
-      ] as Rectangle[];
+      x = 0;
+      y += bin.reduce((sum, rect) => sum + rect.height, 0);
     }
   }
+
+packRectangles(rectangles: Rectangle[], binWidth: number, binHeight: number): Rectangle[][] {
+  const bins: Rectangle[][] = [[]];
+  let currentBinHeight = 0;
+
+  rectangles.sort((a, b) => b.height - a.height);
+
+  for (const rectangle of rectangles) {
+    if (rectangle.width > binWidth || rectangle.height > binHeight) {
+      throw new Error('Rectangle too large for bin');
+    }
+
+    if (currentBinHeight + rectangle.height > binHeight) {
+      bins.push([]);
+      currentBinHeight = 0;
+    }
+
+    bins[bins.length - 1].push(rectangle);
+    currentBinHeight += rectangle.height;
+  }
+
+  return bins;
 }
+
+}
+
+export class Rectangle {
+  width: number;
+  height: number;
+  color: string;
+}
+
